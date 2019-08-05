c8ba2c27178b5aa5a09a5b33b3696ed1ca4c1d3d
Dylan Steck <dylancsteck@gmail.com>
2019-08-05 13:11:46 -0400

    toying around, not working yet

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import '../assets/css/App.css'
 import React, {Component} from 'react';
 import Flow from './Flow/Flow.js';
 import Sidebar from './Sidebar/Sidebar.js'
-// import {BrowserRouter as HashRouter, Route, Redirect, Switch} from "react-router-dom";
 
 export default class App extends Component{
   constructor(props){
@@ -20,19 +19,10 @@ export default class App extends Component{
     }
   }
   render(){
-    // I left the router code for now because I'll need it at some point, but I don't actually need it right now.
     return(
     <div>
-       {/*<HashRouter basename="/">
-        <Switch>
-        */}
       <Sidebar />
       {this.checkCurrentFlow()}
-      {/*<Route exact path="/" component={Sidebar} />
-        <Route path="/flow" component={Flow} />
-        </Switch>
-      </HashRouter>
-      */}
     </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App.js'
+import registerServiceWorker from './registerServiceWorker';
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
@@ -10,3 +11,4 @@ document.body.appendChild(root)
 
 // Now we can render our application into it
 render(<App />, document.getElementById('root'));
+registerServiceWorker();
