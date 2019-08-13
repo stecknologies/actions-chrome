47aa4df17b3650c70e269e07514cca7e305c208c
Dylan Steck <dylancsteck@gmail.com>
2019-08-13 09:05:33 -0400

    Promise issue resolved; tab array pushed properly from Tabs.js

--- src/components/chromeHandlers/Tabs.js
+++ src/components/chromeHandlers/Tabs.js
@@ -1,9 +1,11 @@
 export async function getTabData(){
   let tabs = [];
   await window.chrome.tabs.query({currentWindow: true}, function(data){
-    for(let tab in data){
-      tabs.push(tab.url);
-    }
+    console.log(data);
+    console.log(typeof data[0]);
+    for(let i=0; i<data.length; i++){
+      tabs.push(data[i]);
+    };
   });
   return tabs;
 }
