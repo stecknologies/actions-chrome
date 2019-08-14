c72815bc6ac1dcf8bb204f6d2d560fccc3c0f12a
Dylan Steck <dylancsteck@gmail.com>
2019-08-14 17:11:30 -0400

    Constructing add/remove tab architecture

--- src/components/chromeHandlers/Tabs.js
+++ src/components/chromeHandlers/Tabs.js
@@ -12,8 +12,21 @@ export function createFromHandlers(name){
   promise.then(data => createAction(name, data));
 }
 
-export function focusOnAction(){
+export function getAllTabs(){
+  var promise = new Promise((resolve,reject) =>{
+    window.chrome.tabs.query(resolve);
+  });
+  promise.then(data => focusOnAction())
+}
+
+export function focusOnAction(allTabs){
   // get length of tab query
+  const len = focusOnAction().length;
+  // get length from gun -- get an array of the indicies that you have to delete and delete them
   // remove the indicies in the current action
   window.chrome.tabs.remove()
+}
+
+export function openTabs(){
+  
 }
