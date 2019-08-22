010ed452488161ae8376ce19319294713a1db4ad
Dylan Steck <dylancsteck@gmail.com>
2019-08-22 16:23:24 -0400

    trying to fix tab issue

--- src/components/DisplayActions/Action/Action.js
+++ src/components/DisplayActions/Action/Action.js
@@ -4,9 +4,8 @@ function Action(props){
 	return(
 		<div>
 			<h3>{props.name}</h3><small>{props.isOpen ? "Active" : ""}</small>
-			<a href="x" target="_blank">
-				<p>{props.name}</p>
-			</a>
+			<h6>Tabs</h6>
+			{Object.keys(props.tabs).map(tab => <a href={props.tabs[tab].url}>{props.tabs[tab].title}</a>)}
 		</div>
 	);
 }
--- src/components/gunHandlers/ActionCRUD.js
+++ src/components/gunHandlers/ActionCRUD.js
@@ -1,12 +1,11 @@
 import Gun from 'gun/gun';
 import React from 'react';
-// import 'gun/sea';
+//import 'gun/sea';
 // import 'gun/lib/webrtc';
 // import 'gun/lib/open';
 const db = Gun();
 const currentAction = localStorage.getItem('currentAction');
 export function createAction(name, tab){
-  console.log(tab);
   const newName = tab['title'];
   const action = {
     name: newName,
@@ -15,25 +14,30 @@ export function createAction(name, tab){
   };
   const newAction = db.get(newName).put(action);
   db.get('actions').set(newAction);
-  db.get('actions').get(newName).get('tabs').get(tab['title']).put(tab);
+  const newTab = db.get(tab['title']).put(tab);
+  db.get('actions').get(newName).get('tabs').set(newTab);
+  db.get('actions').get(newName).map().on(data => console.log(data));
   localStorage.setItem('currentAction', newName);
-  //tab.title is name for now
 }
 export function addTabToAction(action, tab){
- db.get('actions').get(action).get('tabs').get(tab['title']).put(tab);
+ const newTab = db.get(tab['title']).put(tab);
+ db.get('actions').get(action).get('tabs').set(newTab);
 }
 
 export function mapActions(){
   var actions = [];
   var counter = 0;
   db.get('actions').map().on(function(data){
+    console.log(data);
     actions.push(data);
+    delete actions[counter]['_'];
+    var tabData = [];
     db.get('actions').get(data['name']).get('tabs').map().on(function(tab){
-    console.log(tab['title']);
-    // delete actions[counter]['tabs'];
-    // actions[counter]['tabs'] = tab;
+      console.log(tab);
+      tabData.push(tab);
+    });
+    actions[counter]['tabs'] = tabData;
     counter ++;
-    })
-  })
+  });
   return actions;
 }
