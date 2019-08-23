64e3c22de7c12e78a2494e3bb82a4271192a8b61
Dylan Steck <dylancsteck@gmail.com>
2019-08-23 11:45:30 -0400

    Close to fixing multi-tab issue
    Not yet stable

--- src/components/gunHandlers/ActionCRUD.js
+++ src/components/gunHandlers/ActionCRUD.js
@@ -2,20 +2,29 @@ import Gun from 'gun/gun';
 import React from 'react';
 var db = Gun();
 //var currentAction = localStorage.getItem('currentAction');
+function newAction(action, name){
+  var actionObj = {[name]: action};
+  var ref = db.get('actionIndex').set(action);
+  return ref;
+}
+function newTab(tab, actionRef){
+  var url = tab['url'];
+  var tabObj = {[url]: tab};
+  var ref = db.get('tabIndex').set(tabObj);
+  actionRef.get('tabs').set(ref);
+  actionRef.get('tabs').get('tabIndex').map().once(d=> console.log(d));
+  return ref;
+}
 export function createAction(name, tab){
-  localStorage.clear();
   console.log(tab);
   var action = {
     name: name,
     createdAt: new Date().toISOString(),
     isOpen: true
   };
-  var newAction = db.get(name).put(action);
-  db.get('actions').set(newAction);
-  var newTab = db.get(tab['url']).put(tab);
-  db.get('actions').get(name).get('tabs').set(newTab);
-  db.get('actions').get(name).get('tabs').map().once(data => console.log(data));
-  localStorage.setItem('currentAction', name);
+  var saveAction = newAction(action, name);
+  // var saveTab = newTab(tab, saveAction);
+  newTab(tab, saveAction);
 }
 export function addTabToAction(action, tab){
  const newTab = db.get(tab['title']).put(tab);
@@ -25,17 +34,17 @@ export function addTabToAction(action, tab){
 export function mapActions(){
   var actions = [];
   var counter = 0;
-  db.get('actions').map().on(function(data){
+  db.get('actionIndex').map().on(function(data){
     console.log(data);
     actions.push(data);
-    delete actions[counter]['_'];
     var tabData = [];
-    db.get('actions').get(data['name']).get('tabs').map().on(function(tab){
+    db.get('actionIndex').get(data).get('tabs').map().on(function(tab){
       console.log(tab);
+      delete actions[counter]['tabs']
       tabData.push(tab);
-    });
-    actions[counter]['tabs'] = tabData;
-    counter ++;
+    })
+  actions[counter]['tabs'] = tabData;
+  counter ++;
   });
   return actions;
 }
