163801abd09271aa975aeb74793b1321f383e33c
Dylan Steck <dylancsteck@gmail.com>
2019-09-09 14:22:45 -0400

    Final build done

--- src/components/App.js
+++ src/components/App.js
@@ -100,9 +100,6 @@ export default class App extends Component{
       localStorage.removeItem('actions');
       console.log(JSON.parse(localStorage.getItem('actions')));
     }
-    // ReactGA.initialize('UA-147084998-2');
-    // ReactGA.pageview(window.location.pathname + window.location.search);
-    // console.log(window.location.pathname + window.location.search);
   }
   render(){
     return(
